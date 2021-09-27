38e6ef229f6f3ec7057efd3594815be1a0ad41f4
Tom Titcombe <t.j.titcombe@gmail.com>
2021-09-27 18:12:58 +0100

    Create source code directory
    Fake carbon intensity API call

---
+++ heimdall/carbon_intensity.py
@@ -0,0 +1,13 @@
+"""
+Call carbon intensity APIs
+"""
+import requests
+
+
+def get_intensity() -> float:
+    response = requests.get("https://FAKEAPI.carbon/")
+
+    if response.status_code != 200:
+        raise RuntimeError(f"Error fetching intensity: {response.json()}")
+    else:
+        return response.json()["intensity"]
