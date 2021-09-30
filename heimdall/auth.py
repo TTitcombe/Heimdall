c6ea26875663540769ca68a1dd6f9146a53929ee
Tom Titcombe <t.j.titcombe@gmail.com>
2021-09-30 08:05:37 +0100

    Fix bad password with casing avoids invalidation bug
    e.g. 'pASSword' used to get validated

--- heimdall/auth.py
+++ heimdall/auth.py
@@ -2,13 +2,13 @@
 Monitoring service authentication
 """
 def validate_password(username, potential_password):
-    if potential_password == username:
+    if potential_password.lower() == username.lower():
         return False
     elif len(potential_password) < 8:
         return False
     elif len(potential_password) > 256:
         return False
-    elif potential_password in ("password", "12345678", "heimdall"):
+    elif potential_password.lower() in ("password", "12345678", "heimdall"):
         return False
     else:
         return True
